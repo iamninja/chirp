c6fe40d5a6c8a32bedfac4c67b84ad5ea507af5d
Vagios Vlachos <bagios234@gmail.com>
2014-02-22 04:25:39 +0200

    Added tests for user first name, last name and profile name correctness.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,31 +3,33 @@ class UserTest < ActiveSupport::TestCase
 # test "the truth" do
 # assert true
 # end
-test "a user should enter a first name" do
-user = User.new
-assert !user.save
-assert !user.errors[:first_name].empty?
-end
-test "a user should enter a last name" do
-user = User.new
-assert !user.save
-assert !user.errors[:last_name].empty?
-end
-test "a user should enter a profile name" do
-user = User.new
-assert !user.save
-assert !user.errors[:profile_name].empty?
-end
-test "a user should have a unique name" do
-user = User.new
-user.profile_name = 'robotron'
-user.email  = "ro@bot.tron"
-user.first_name = "Ro"
-user.last_name = "Bot"
-user.password = "password"
-user.password_confiramation = "password"
-assert !user.save
-puts user.errors.inspect
-assert !user.errors[:profile_name].empty?
-end
+	test "a user should enter a first name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:first_name].empty?
+	end
+	test "a user should enter a last name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:last_name].empty?
+	end
+	test "a user should enter a profile name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+	test "a user should have a unique profile name" do
+		user = User.new
+		user.profile_name = users(:ro).profile_name
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+	test "a user should have a profile name without spaces" do
+		user = User.new
+		user.profile_name = "My Profile With Spaces"
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+		assert user.errors[:profile_name].include?(" must be formatted correctly.")
+	end
 end

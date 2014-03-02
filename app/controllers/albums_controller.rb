318c65f6945c73bc56870a52463126023e8759c5
Vagios Vlachos <bagios234@gmail.com>
2014-03-02 10:01:43 +0200

    Added gem twitter-bootstrap-rails. Added breadcrumbs on albums-pictures pages. Minor layout fixes.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,6 +3,7 @@ class AlbumsController < ApplicationController
   before_filter :authenticate_user!, only: [:create, :new, :update, :edit, :destroy]
   before_filter :find_user
   before_filter :find_album, only: [:edit, :update, :destroy, :show]
+  before_filter :add_breadcrumbs
   # GET /albums
   # GET /albums.json
   def index
@@ -39,6 +40,7 @@ class AlbumsController < ApplicationController
   # GET /albums/1/edit
   def edit
     @album = current_user.albums.find(params[:id])
+    add_breadcrumb "Editing Album"
   end
 
   # POST /albums
@@ -90,6 +92,10 @@ class AlbumsController < ApplicationController
   end
 
   private
+    def add_breadcrumbs
+      add_breadcrumb @user, profile_path(@user)
+      add_breadcrumb "Albums", albums_path
+    end
     def find_user
       @user = User.find_by_profile_name(params[:profile_name])
     end
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,6 +2,7 @@ class PicturesController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_filter :find_user
   before_filter :find_album
+  before_filter :add_breadcrumbs
   # GET /pictures
   # GET /pictures.json
   def index
@@ -16,7 +17,8 @@ class PicturesController < ApplicationController
   # GET /pictures/1
   # GET /pictures/1.json
   def show
-    @picture = Picture.find(params[:id])
+    @picture = @album.pictures.find(params[:id])
+    add_breadcrumb @picture, album_picture_path(@album, @picture)
 
     respond_to do |format|
       format.html # show.html.erb
@@ -90,6 +92,11 @@ class PicturesController < ApplicationController
   end
 
   private
+  def add_breadcrumbs
+    add_breadcrumb @user.first_name, profile_path(@user)
+    add_breadcrumb "Albums", albums_path
+    add_breadcrumb "Pictures", album_pictures_path(@album)
+  end
   def find_user
     @user = User.find_by_profile_name(params[:profile_name])
   end
